b5c55bcb450cb264aecb82110e72fcaeb719a5d1
Xavier Hamel <xavier.hamel.5@ulaval.ca>
2024-03-15 18:48:15 -0400

    Connecting

--- source/index.js
+++ source/index.js
@@ -1,20 +1,30 @@
+import query from "./queryProcess.js"; 
+
 const chatForm = get('form');
 const chatInput = get('input');
 const chatBox = get('main');
 
-appendMessage('bot', 'This is a bot bubble');
-appendMessage('user', 'This is a user bubble');
 
-chatForm.addEventListener('submit', event => {
+appendMessage('bot', 'This is a bot bubble');
+
+chatForm.addEventListener('submit', async (event) =>  {
   event.preventDefault();
   const text = chatInput.value;
   if (!text) return;
   
   appendMessage('user', text);
   chatInput.value = '';
+
+  const response = await query({
+    inputs: text,
+    parameters:{}
+  });
+
+  appendMessage('bot', response);
 });
 
 function appendMessage(side, text) {
+  console.log("append MEssage");
   const bubble = `
     <div class="msg -${side}">
         <div class="bubble">${text}</div>
---
+++ source/queryProcess.js
@@ -0,0 +1,27 @@
+export default async function query(data) {
+	const response = await fetch(
+		"https://xevhza5rhd1jhkq8.us-east-1.aws.endpoints.huggingface.cloud",
+		{
+			headers: { 
+				"Accept" : "application/json",
+				"Content-Type": "application/json" 
+			},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	console.log("result");
+	return result;
+}
+
+
+
+query({
+    "inputs": "This is the text sent to the model",
+    "parameters": {}
+}).then((response) => {
+	console.log(JSON.stringify(response));
+});
+
+
