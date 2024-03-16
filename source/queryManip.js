e38ab3aa05aa59ea52aca63d75c9281cc133df63
Xavier Hamel <xavier.hamel.5@ulaval.ca>
2024-03-15 20:57:37 -0400

    yep

--- source/queryManip.js
+++ source/queryManip.js
@@ -38,16 +38,17 @@ export class Conversation{
                 symptoms_text += test[1];
             }
         });
-        let enough_info_response = query({"inputs": "Based on the information below. Is this enough information to give good recommendations on what to do and even diagnosis? If yes then reply YES, if no then reply NO. You MUST not reply anything else." + symptoms_text,
+        let enough_info_response = query({"inputs": "Based on the information below. Is this enough information to give good medical recommendations on what to do and even diagnosis? If yes then reply YES, if no then reply NO. You MUST not reply anything other than YES or NO. Patient symptoms: \n" + symptoms_text + "End of the user symptoms. YES or NO ?",
                                                 "parameters": {}});
         let additionnal_info = query({"inputs": "You are Eve, a chatbot designed to help patients by guiding them towards appropriate care services. Eve MUST be be helpful at all times. With the information below, write a polite reply requesting more information. You should specify what would help give a better diagnosis." + symptoms_text,
         "parameters":{}});
         
-    enough_info_response = await enough_info_response;
-    let enough_info_response_text = enough_info_response[0].generated_text;
+    let enough_info_response_done = await enough_info_response;
+    console.log(enough_info_response_done)
+    let enough_info_response_text = enough_info_response_done[0].generated_text;
     console.log(enough_info_response_text);
 
-    if(enough_info_response.includes("YES") || enough_info_response.includes("yes")){
+    if(enough_info_response_text.includes("YES") || enough_info_response_text.includes("yes")){
         const intructions = "You are Eve, a chatbot designed to help patients by guiding them towards appropriate care services. Eve MUST be be helpful at all times. If you are uncertain, recommend consulting a human medical professional. You must only complete what Eve will say. Not the user. Below is the text from a patient describing their symptoms. First say whether the patient requires immediate care, then give a diagnosis and recommendations on what to do. Use the text below.";
         const prompt = intructions + symptoms_text + "Eve:";
 
